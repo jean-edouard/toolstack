cd1c067d28dbf859ba6182f5981cfde2044e91f4
Chris Rogers <rogersc@ainfosec.com>
2016-09-14 10:58:46 -0400

    Debugging for potential domain not_found
    Signed-off-by: Chris Rogers <rogersc@ainfosec.com>
    (cherry picked from commit 5cf7eb687261666ebb8886f8cfe5c6d15c68c176)
    Signed-off-by: Jed <lejosnej@ainfosec.com>

--- xenstored/xenstored.ml
+++ xenstored/xenstored.ml
@@ -49,6 +49,7 @@ let process_connection_fds store cons domains rset wset =
 
 let process_domains store cons domains =
 	let do_io_domain domain =
+		debug "do_io_domain: %s  domain:%s" (do_io_domain) (domain);
 		let con = Connections.find_domain cons (Domain.get_id domain) in
 		Process.do_input store cons domains con;
 		Process.do_output store cons domains con in
